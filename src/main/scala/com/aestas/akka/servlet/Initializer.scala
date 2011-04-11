66c8c387f4ecb38261edc233264b9bc73c7574a1
Luciano Fiandesio <luciano@aestasit.com>
2011-04-11 11:47:17 +0200

    first commit

---
+++ src/main/scala/com/aestas/akka/servlet/Initializer.scala
@@ -0,0 +1,29 @@
+package com.aestas.akka.servlet
+import akka.remote.BootableRemoteActorService
+import akka.actor.BootableActorLoaderService
+import akka.config.Config
+//import akka.util.{Logging, Bootable}
+import akka.util.AkkaLoader
+
+import javax.servlet.{ServletContextListener, ServletContextEvent}
+
+ /**
+  * This class can be added to web.xml mappings as a listener to start and postStop Akka.
+  *
+  *<web-app>
+  * ...
+  *  <listener>
+  *    <listener-class>akka.servlet.Initializer</listener-class>
+  *  </listener>
+  * ...
+  *</web-app>
+  */
+class Initializer extends ServletContextListener {
+   lazy val loader = new AkkaLoader
+
+   def contextDestroyed(e: ServletContextEvent): Unit =
+     loader.shutdown
+
+   def contextInitialized(e: ServletContextEvent): Unit =
+     loader.boot(true, new BootableActorLoaderService with BootableRemoteActorService)
+ }
